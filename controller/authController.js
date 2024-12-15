d4484b6072fc7af296c6c13f7807f41458b21b40
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-16 01:31:19 +0530

    changed the token signing with id to email

--- controller/authController.js
+++ controller/authController.js
@@ -25,7 +25,7 @@ const loginUser = async(req, res) =>{
         if (!user) return res.status(404).json({ message: "User not found" });
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        const token = jwt.sign({email: user.email}, process.env.JWT_SECRET, {expiresIn: '1h'})
         res.status(200).json({ token });
     } catch (err) {
         res.status(500).json({ message: err.message });
